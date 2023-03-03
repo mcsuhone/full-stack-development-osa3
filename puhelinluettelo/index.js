42d1a56f3788b19e2c392ef6df73b6fd8a75b64c
mcsuhone <mikko.e.suhonen@aalto.fi>
2023-03-03 12:36:49 +0200

    3.9.-3.11.

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
-var morgan = require('morgan')
+const cors = require('cors')
+// var morgan = require('morgan')
 const app = express()
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
+// morgan.token('body', (req, res) => JSON.stringify(req.body));
+// app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 
 app.use(express.json())
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 let persons = [
     {
@@ -97,7 +102,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
